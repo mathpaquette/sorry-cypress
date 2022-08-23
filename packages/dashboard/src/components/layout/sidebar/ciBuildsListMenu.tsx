04ab7f1be3fa89d8a4de5a15dd0a4b23d8a6d569
Math Paquette <me@mathpaquette.com>
2022-08-22 20:20:20 -0400

    refactor: innerListItemMenu

--- packages/dashboard/src/components/layout/sidebar/ciBuildsListMenu.tsx
+++ packages/dashboard/src/components/layout/sidebar/ciBuildsListMenu.tsx
@@ -1,23 +1,14 @@
 import { PlayLesson as PlayLessonIcon } from '@mui/icons-material';
-import {
-  alpha,
-  IconButton,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Tooltip,
-} from '@mui/material';
+import { InnerListItemMenu } from '@sorry-cypress/dashboard/components/layout/sidebar/innerListItemMenu';
 import { NavItemType } from '@sorry-cypress/dashboard/lib/navigation';
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
 
 export const CiBuildsListMenu: CiBuildsListMenuType = ({
-  projectColor,
   open,
   onItemClick,
   selectedItem,
 }) => {
-  const projectMenuItems = [
+  const menuItems = [
     {
       label: 'Latest builds',
       link: `/ci-builds`,
@@ -27,72 +18,15 @@ export const CiBuildsListMenu: CiBuildsListMenuType = ({
   ];
   return (
     <>
-      {projectMenuItems.map((item) => {
-        const selected = selectedItem === item.type;
-        if (open) {
-          return (
-            <ListItemButton
-              key={item.label}
-              color={selected ? 'primary' : undefined}
-              sx={{
-                ml: 2,
-                mb: 1,
-                mr: { md: 0, xs: 2 },
-                pl: 2,
-                '&.Mui-selected': {
-                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                },
-                backgroundColor: selected
-                  ? 'rgba(255, 255, 255, 0.1)'
-                  : undefined,
-                borderTopLeftRadius: 8,
-                borderTopRightRadius: { md: 0, xs: 8 },
-                borderBottomRightRadius: { md: 0, xs: 8 },
-                borderBottomLeftRadius: 8,
-              }}
-              component={RouterLink}
-              to={item.link}
-              selected={selected}
-              onClick={onItemClick}
-            >
-              <ListItemIcon sx={{ opacity: selected ? 1 : 0.6, minWidth: 28 }}>
-                <item.iconComponent color={selected ? 'primary' : undefined} />
-              </ListItemIcon>
-              <ListItemText
-                primary={item.label}
-                primaryTypographyProps={{
-                  fontSize: 16,
-                  color: selected ? 'primary' : 'text.primary',
-                  sx: { opacity: selected ? 1 : 0.6 },
-                }}
-              />
-            </ListItemButton>
-          );
-        }
-
+      {menuItems.map((item) => {
         return (
-          <Tooltip key={item.label} title={item.label} placement="right" arrow>
-            <div style={{ paddingBottom: '8px' }}>
-              <IconButton
-                aria-label={item.label}
-                color={selected ? 'primary' : undefined}
-                sx={{
-                  opacity: selected ? 1 : 0.6,
-                  padding: 1.5,
-                  backgroundColor: selected
-                    ? alpha(projectColor || '#3486E3', 0.1)
-                    : undefined,
-                  color: selected ? projectColor || '#3486E3' : undefined,
-                }}
-                component={RouterLink}
-                to={item.link}
-                onClick={onItemClick}
-                size="large"
-              >
-                <item.iconComponent />
-              </IconButton>
-            </div>
-          </Tooltip>
+          <InnerListItemMenu
+            key={item.label}
+            item={item}
+            open={open}
+            onItemClick={onItemClick}
+            selectedItem={selectedItem}
+          ></InnerListItemMenu>
         );
       })}
     </>
@@ -100,7 +34,6 @@ export const CiBuildsListMenu: CiBuildsListMenuType = ({
 };
 
 type CiBuildsListMenuProps = {
-  projectColor?: string;
   open: boolean;
   onItemClick: () => void;
   selectedItem: NavItemType;
--- packages/dashboard/src/components/layout/sidebar/projectDetailsMenu.tsx
+++ packages/dashboard/src/components/layout/sidebar/projectDetailsMenu.tsx
@@ -2,17 +2,9 @@ import {
   PlayLesson as PlayLessonIcon,
   Settings as SettingsIcon,
 } from '@mui/icons-material';
-import {
-  alpha,
-  IconButton,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-  Tooltip,
-} from '@mui/material';
+import { InnerListItemMenu } from '@sorry-cypress/dashboard/components/layout/sidebar/innerListItemMenu';
 import { NavItemType } from '@sorry-cypress/dashboard/lib/navigation';
 import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
 
 export const ProjectDetailsMenu: ProjectDetailsMenuType = ({
   projectId,
@@ -21,7 +13,7 @@ export const ProjectDetailsMenu: ProjectDetailsMenuType = ({
   onItemClick,
   selectedItem,
 }) => {
-  const projectMenuItems = [
+  const menuItems = [
     {
       label: 'Latest runs',
       link: `/${encodeURIComponent(projectId)}/runs`,
@@ -37,72 +29,16 @@ export const ProjectDetailsMenu: ProjectDetailsMenuType = ({
   ];
   return (
     <>
-      {projectMenuItems.map((item) => {
-        const selected = selectedItem === item.type;
-        if (open) {
-          return (
-            <ListItemButton
-              key={item.label}
-              color={selected ? 'primary' : undefined}
-              sx={{
-                ml: 2,
-                mb: 1,
-                mr: { md: 0, xs: 2 },
-                pl: 2,
-                '&.Mui-selected': {
-                  backgroundColor: 'rgba(255, 255, 255, 0.1)',
-                },
-                backgroundColor: selected
-                  ? 'rgba(255, 255, 255, 0.1)'
-                  : undefined,
-                borderTopLeftRadius: 8,
-                borderTopRightRadius: { md: 0, xs: 8 },
-                borderBottomRightRadius: { md: 0, xs: 8 },
-                borderBottomLeftRadius: 8,
-              }}
-              component={RouterLink}
-              to={item.link}
-              selected={selected}
-              onClick={onItemClick}
-            >
-              <ListItemIcon sx={{ opacity: selected ? 1 : 0.6, minWidth: 28 }}>
-                <item.iconComponent color={selected ? 'primary' : undefined} />
-              </ListItemIcon>
-              <ListItemText
-                primary={item.label}
-                primaryTypographyProps={{
-                  fontSize: 16,
-                  color: selected ? 'primary' : 'text.primary',
-                  sx: { opacity: selected ? 1 : 0.6 },
-                }}
-              />
-            </ListItemButton>
-          );
-        }
-
+      {menuItems.map((item) => {
         return (
-          <Tooltip key={item.label} title={item.label} placement="right" arrow>
-            <div style={{ paddingBottom: '8px' }}>
-              <IconButton
-                aria-label={item.label}
-                color={selected ? 'primary' : undefined}
-                sx={{
-                  opacity: selected ? 1 : 0.6,
-                  padding: 1.5,
-                  backgroundColor: selected
-                    ? alpha(projectColor || '#3486E3', 0.1)
-                    : undefined,
-                  color: selected ? projectColor || '#3486E3' : undefined,
-                }}
-                component={RouterLink}
-                to={item.link}
-                onClick={onItemClick}
-                size="large"
-              >
-                <item.iconComponent />
-              </IconButton>
-            </div>
-          </Tooltip>
+          <InnerListItemMenu
+            key={item.label}
+            projectColor={projectColor}
+            item={item}
+            open={open}
+            onItemClick={onItemClick}
+            selectedItem={selectedItem}
+          ></InnerListItemMenu>
         );
       })}
     </>
